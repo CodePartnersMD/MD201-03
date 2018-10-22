6dd9fbbfe7ebb1e0dc3e81ced2cf03e63671903a
NAME <EMAIL>
2018-10-22 16:00:03 -0400

    pushing demo for lab 1

---
+++ class-01-toolbelt-terminal-git/demo/main.js
@@ -0,0 +1,13 @@
+
+let favoriteHero = prompt('Who is your favorite Hero?')
+
+console.log('My favorite Hero is ', favoriteHero)
+
+let universe = confirm('Do you follow Marvel?')
+
+console.log(universe)
+
+
+
+
+
